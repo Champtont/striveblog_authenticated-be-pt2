c0f9eba68f81eaade4bfb3fd389a83d0a88d67fd
Courtney Hampton <jaye0888@gmail.com>
2023-02-08 16:05:50 +0100

    implementing JWT

--- src/api/authors/index.js
+++ src/api/authors/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import createHttpError from "http-errors";
+import { adminOnlyMiddleware } from "../../lib/auth/adminOnly.js";
+import { JWTAuthMiddleware } from "../../lib/auth/jwtAuth.js";
+import { createAccessToken } from "../../lib/auth/tools.js";
 import AuthorModel from "./model.js";
 
 const authorsRouter = express.Router();
@@ -14,34 +17,33 @@ authorsRouter.post("/", async (req, res, next) => {
   }
 });
 
-authorsRouter.get("/", async (req, res, next) => {
-  try {
-    const authors = await AuthorModel.find();
-    res.send(authors);
-  } catch (error) {
-    next(error);
-  }
-});
-
-authorsRouter.get("/:authorId", async (req, res, next) => {
-  try {
-    const author = await AuthorModel.findById(req.params.authorId);
-    if (author) {
+authorsRouter.get(
+  "/",
+  JWTAuthMiddleware,
+  adminOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+      const author = await AuthorModel.find();
       res.send(author);
-    } else {
-      next(
-        createHttpError(404, `Author with id ${req.params.authorId} not found!`)
-      );
+    } catch (error) {
+      next(error);
     }
+  }
+);
+
+authorsRouter.get("/me", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const author = await AuthorModel.findById(req.author._id);
+    res.send(author);
   } catch (error) {
     next(error);
   }
 });
 
-authorsRouter.put("/:authorId", async (req, res, next) => {
+authorsRouter.put("/me", JWTAuthMiddleware, async (req, res, next) => {
   try {
     const updatedAuthor = await AuthorModel.findByIdAndUpdate(
-      req.params.authorId,
+      req.author._id,
       req.body,
       { new: true, runValidators: true }
     );
@@ -57,17 +59,72 @@ authorsRouter.put("/:authorId", async (req, res, next) => {
   }
 });
 
-authorsRouter.delete("/:authorId", async (req, res, next) => {
+authorsRouter.delete("/me", JWTAuthMiddleware, async (req, res, next) => {
   try {
-    const deletedAuthor = await AuthorModel.findByIdAndDelete(
-      req.params.authorId
-    );
+    const deletedAuthor = await AuthorModel.findByIdAndDelete(req.author._id);
     if (deletedAuthor) {
       res.status(204).send();
     } else {
       next(
         createHttpError(404, `Author with id ${req.params.authorId} not found!`)
       );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+authorsRouter.get("/:authorId", JWTAuthMiddleware, async (req, res, next) => {
+  try {
+    const author = await AuthorModel.findById(req.params.authorId);
+    if (author) {
+      res.send(author);
+    } else {
+      next(
+        createHttpError(404, `Author with id ${req.params.authorId} not found!`)
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+authorsRouter.put(
+  "/:authorId",
+  JWTAuthMiddleware,
+  adminOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+authorsRouter.delete(
+  "/:authorId",
+  JWTAuthMiddleware,
+  adminOnlyMiddleware,
+  async (req, res, next) => {
+    try {
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+authorsRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const author = await AuthorModel.checkCredentials(email, password);
+
+    if (author) {
+      const payload = { _id: author._id, role: author.role };
+
+      const accessToken = await createAccessToken(payload);
+      res.send({ accessToken });
+    } else {
+      next(createHttpError(401, "Credentials are not ok!"));
     }
   } catch (error) {
     next(error);
